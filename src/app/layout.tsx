d3b1645642c5dd21b883566c2e6b1d842f66e967
Kunal Rana <kunalranarj2005@gmail.com>
2025-12-18 12:50:24 +0530

    updated color theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,13 @@
 /** @format */
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Nunito_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import ReactQueryProvider from "@/components/ReactQueryProvider";
 import { Toaster } from "@/components/ui/sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const nunitoSans = Nunito_Sans({ variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "ABN - Home",
@@ -29,10 +21,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={nunitoSans.variable}>
+      <body className={`${nunitoSans.variable} antialiased`}>
         <ReactQueryProvider>
           <Toaster richColors />
           <Navbar />
